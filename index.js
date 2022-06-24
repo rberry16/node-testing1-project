2654116445d9d602f9bdbcccf51a15245dcc7d89
Ryan Berry <rberry16@hotmail.com>
2022-06-24 16:34:42 -0700

    8 tests passing

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ class Counter {
    */
   constructor(initialNumber) {
     // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -70,7 +71,11 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    if (this.count > 0) {
+      return this.count--;
+    } else {
+      return this.count;
+    }
   }
 }
 
