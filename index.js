54f910efbb2dfe35a5d293847393cdde55d3e696
Ryan Berry <rberry16@hotmail.com>
2022-06-24 15:54:49 -0700

    3 tests passing

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ function trimProperties(obj) {
  * trimPropertiesMutation({ name: '  jane  ' }) // returns the object mutated in place { name: 'jane' }
  */
 function trimPropertiesMutation(obj) {
-  // ✨ implement
+for (let prop in obj) {
+  obj[prop] = obj[prop].trim();
+}
+return obj;
 }
 
 /**
@@ -36,7 +39,13 @@ function trimPropertiesMutation(obj) {
  * findLargestInteger([{ integer: 1 }, { integer: 3 }, { integer: 2 }]) // returns 3
  */
 function findLargestInteger(integers) {
-  // ✨ implement
+  let result = integers[0].integer;
+  for (let i = 0; i < integers.length; i++) {
+    if (integers[i].integer > result) {
+      result = integers[i].integer;
+    }
+  }
+  return result;
 }
 
 class Counter {
